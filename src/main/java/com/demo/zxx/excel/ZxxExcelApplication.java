ded24951a6116debea8c7e3a77e44c72a61a0a7f
ZhangXiaoxiang <1090510535@qq.com>
2019-04-19 00:57:10 +0800

    excel批量操作demo

---
+++ src/main/java/com/demo/zxx/excel/ZxxExcelApplication.java
@@ -0,0 +1,13 @@
+package com.demo.zxx.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxxExcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZxxExcelApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/demo/zxx/excel/excel/ExcelException.java
@@ -0,0 +1,14 @@
+package com.demo.zxx.excel.excel;
+
+/**
+ * Created with IntelliJ IDEA
+ *
+ * @Author
+ * @Description Excel 解析 Exception
+
+ */
+public class ExcelException extends RuntimeException {
+    public ExcelException(String message) {
+        super(message);
+    }
+}
