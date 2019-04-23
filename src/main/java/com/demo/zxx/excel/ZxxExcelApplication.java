e03764eec08c08aab71ce6db0cef83b91aa157d4
BF-20180513XZNU\Administrator <475243015@qq.com>
2019-04-23 15:40:06 +0800

    端口8081

--- src/main/java/com/demo/zxx/excel/ZxxExcelApplication.java
+++ src/main/java/com/demo/zxx/excel/ZxxExcelApplication.java
@@ -10,9 +10,9 @@ public class ZxxExcelApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZxxExcelApplication.class, args);
-        System.out.println("模拟数据导出数据库请访问: http://localhost:8080/writeExcel ");
-        System.out.println("导出数据库请访问 http://localhost:8080/cardmi/export");
-        System.out.println("导入数据库请访问 http://localhost:8080/index");
+        System.out.println("模拟数据导出数据库请访问: http://localhost:8081/writeExcel ");
+        System.out.println("导出数据库请访问 http://localhost:8081/cardmi/export");
+        System.out.println("导入数据库请访问 http://localhost:8081/index");
     }
 
 }
